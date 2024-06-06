fca13b5cc750ca4c14d375f2884b414bc59967f3
Lazare Fortune <lazarefortune@gmail.com>
2024-06-06 22:24:04 +0200

    fix: contact page missing admin guard
    Fix #2

--- src/Http/Admin/Controller/ContactController.php
+++ src/Http/Admin/Controller/ContactController.php
@@ -5,8 +5,10 @@ namespace App\Http\Admin\Controller;
 use App\Domain\Contact\Repository\ContactRepository;
 use App\Http\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route( '/contact', name: 'contact_' )]
+#[IsGranted('ROLE_ADMIN')]
 class ContactController extends AbstractController
 {
     public function __construct(
