2562e3d547ced0df7338b9b39bae581c03c75fb0
Lazare Fortune <lazarefortune@gmail.com>
2024-11-04 01:58:24 +0100

    Add OAuth2 integration for Google and GitHub login options in account profile

--- src/Http/Controller/Social/SocialLoginController.php
+++ src/Http/Controller/Social/SocialLoginController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controller\Social;
 
+use App\Domain\Auth\Core\Entity\User;
+use App\Domain\Auth\Core\Repository\UserRepository;
 use App\Http\Controller\AbstractController;
 use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class SocialLoginController extends AbstractController
 {
@@ -33,6 +36,20 @@ class SocialLoginController extends AbstractController
         }
     }
 
+    #[Route(path: '/oauth/unlink/{service}', name: 'oauth_unlink')]
+    #[IsGranted('ROLE_USER')]
+    public function disconnect(string $service, UserRepository $userRepository): RedirectResponse
+    {
+        $this->ensureServiceAccepted($service);
+        $method = 'set'.ucfirst($service).'Id';
+        /** @var User $user */
+        $user = $this->getUser();
+        $user->$method(null);
+        $userRepository->save($user, true);
+        $this->addFlash('success', 'Votre compte a bien été dissocié de '.$service);
+
+        return $this->redirectToRoute('app_account_profile');
+    }
 
     #[Route(path: '/oauth/check/{service}', name: 'oauth_check')]
     public function check(): Response
