3df260c3596a58f3197b252cfb29591fbe97d6fd
Lazare Fortune <lazarefortune@gmail.com>
2024-11-11 03:52:35 +0100

    Add email test form and functionality to admin dashboard

---
+++ src/Domain/Application/Form/EmailTestForm.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Domain\Application\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class EmailTestForm extends AbstractType
+{
+    public function buildForm( FormBuilderInterface $builder, array $options ) : void
+    {
+        $builder->add('email', EmailType::class, [
+            'attr' => [
+                'class' => 'form-input',
+                'autocomplete' => 'off',
+                'placeholder' => 'Email',
+            ]
+        ]);
+    }
+}
--- src/Http/Admin/Controller/HomeController.php
+++ src/Http/Admin/Controller/HomeController.php
@@ -2,17 +2,23 @@
 
 namespace App\Http\Admin\Controller;
 
+use App\Domain\Application\Form\EmailTestForm;
 use App\Domain\Auth\Core\Repository\UserRepository;
 use App\Domain\Course\CourseService;
 use App\Domain\Course\Service\FormationService;
 use App\Http\Controller\AbstractController;
+use App\Infrastructure\Mailing\MailService;
 use App\Infrastructure\Youtube\YoutubeService;
 use Psr\Cache\InvalidArgumentException;
 use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Contracts\Cache\ItemInterface;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
 #[IsGranted( 'ROLE_ADMIN' )]
 class HomeController extends AbstractController
@@ -30,8 +36,8 @@ class HomeController extends AbstractController
     /**
      * @throws InvalidArgumentException
      */
-    #[Route( '/', name: 'home', methods: ['GET'] )]
-    public function index() : Response
+    #[Route( '/', name: 'home', methods: ['GET', 'POST'] )]
+    public function index( Request $request, MailService $mailService ) : Response
     {
         $cache = new FilesystemAdapter();
 
@@ -67,13 +73,36 @@ class HomeController extends AbstractController
         $dailyUsersLast30Days = $this->userRepository->countDailyUsersLast30Days();
         $monthlyUsersLast24Months = $this->userRepository->countMonthlyUsersLast24Months();
 
+        $formTestEmail = $this->createForm( EmailTestForm::class );
+        $formTestEmail->handleRequest( $request );
+
+        if ( $formTestEmail->isSubmitted() && $formTestEmail->isValid() ) {
+            $data = $formTestEmail->getData();
+
+            $emailTo = $data['email'];
+
+            try {
+                $email = $mailService->createEmail( 'mails/test.twig' , [])
+                    ->to( $emailTo )
+                    ->subject( 'Email de test' );
+
+                $mailService->send( $email );
+
+                $this->addFlash('success', 'Message envoyé avec succès');
+            } catch ( LoaderError|RuntimeError|SyntaxError $e ) {
+                $this->addFlash( 'error', $e->getMessage() );
+            }
+
+        }
+
         return $this->render( 'pages/admin/index.html.twig', [
             'youtubeSubscribersCount' => $youtubeSubscribersCount,
             'countUsers'         => $countUsers,
             'countOnlineCourses' => $countOnlineCourses,
             'countOnlineFormations' => $countOnlineFormations,
             'dailyUsersLast30Days' => $dailyUsersLast30Days,
-            'monthlyUsersLast24Months' => $monthlyUsersLast24Months
+            'monthlyUsersLast24Months' => $monthlyUsersLast24Months,
+            'formTestEmail' => $formTestEmail->createView(),
         ] );
     }
 
