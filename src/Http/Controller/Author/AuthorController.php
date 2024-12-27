2af4c63b0c4abd70c3b6c30f3fa924cdaa036726
Lazare Fortune <lazarefortune@gmail.com>
2024-12-28 00:35:41 +0100

    chore: Refactor UI and routes for clarity and update avatar link for consistency

--- src/Http/Controller/Author/AuthorController.php
+++ src/Http/Controller/Author/AuthorController.php
@@ -8,7 +8,7 @@ use App\Http\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/profile', name: 'author_')]
+#[Route('/auteur', name: 'author_')]
 class AuthorController extends AbstractController
 {
     public function __construct(
@@ -20,19 +20,16 @@ class AuthorController extends AbstractController
     #[Route('/{id}', name: 'show', methods: ['GET'])]
     public function showAuthor( User $user ) : Response
     {
-        # check user role
-        if ( $this->hasRole($user, 'ROLE_AUTHOR') ) {
-            $courses = $this->courseRepository->findBy(['author' => $user], ['publishedAt' => 'DESC']);
-
-            return $this->render('pages/public/author/show.html.twig', [
-                'author' => $user,
-                'courses' => $courses,
-            ]);
+        if ( !$this->hasRole($user, 'ROLE_AUTHOR') ) {
+            // redirect to 404
+            throw $this->createNotFoundException('Auteur non trouvé');
         }
 
+        $courses = $this->courseRepository->findBy(['author' => $user], ['publishedAt' => 'DESC']);
 
-        return $this->render('pages/public/users/show.html.twig', [
-            'user' => $user,
+        return $this->render('pages/public/author/show.html.twig', [
+            'author' => $user,
+            'courses' => $courses,
         ]);
     }
 }
