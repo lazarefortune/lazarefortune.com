a8f53a032c1eafb497897834eccca204c882ba8f
Lazare Fortune <lazarefortune@gmail.com>
2025-01-16 13:59:37 +0100

    chore: refactor commands to use attributes and tweak config for better performance.

--- src/Command/BadgeUnlockCommand.php
+++ src/Command/BadgeUnlockCommand.php
@@ -3,15 +3,15 @@
 namespace App\Command;
 
 use App\Domain\Badge\BadgeService;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
+#[AsCommand(name: 'app:badge-unlock')]
 class BadgeUnlockCommand extends Command
 {
-    protected static $defaultName = 'app:badge-unlock';
-
     public function __construct(
     )
     {
--- src/Command/DailyCronCommand.php
+++ src/Command/DailyCronCommand.php
@@ -2,15 +2,15 @@
 
 namespace App\Command;
 
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
+#[AsCommand(name: 'app:daily-cron')]
 class DailyCronCommand extends Command
 {
-    protected static $defaultName = 'app:daily-cron';
-
     public function __construct()
     {
         parent::__construct();
