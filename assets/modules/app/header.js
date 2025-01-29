e19be699747b1ab1a848fdecbc95eaa7231bd42e
Lazare Fortune <lazarefortune@gmail.com>
2025-01-29 22:17:18 +0100

    chore: Refactor dropdown and button styles for improved dark mode and interactivity

--- assets/modules/app/header.js
+++ assets/modules/app/header.js
@@ -85,4 +85,27 @@ document.addEventListener('DOMContentLoaded', () => {
         hamburger.classList.toggle('is-open');
         document.body.style.overflow = menu.classList.contains('active') ? 'hidden' : 'auto';
     });
+
+    /* --- User Dropdown --- */
+    const headerDropdown = document.querySelector('#header-dropdown-button');
+    const headerDropdownMenu = document.querySelector('#header-dropdown-menu');
+
+    if (headerDropdown && headerDropdownMenu) {
+        headerDropdown.addEventListener('click', (event) => {
+            event.stopPropagation(); // Empêche la propagation du clic au document
+
+            // Bascule la visibilité du menu déroulant associé
+            headerDropdownMenu.classList.toggle('is-visible');
+
+            // Met à jour l'attribut aria-expanded pour l'accessibilité
+            const isExpanded = headerDropdown.getAttribute('aria-expanded') === 'true';
+            headerDropdown.setAttribute('aria-expanded', !isExpanded);
+        });
+
+        document.addEventListener('click', () => {
+            headerDropdownMenu.classList.remove('is-visible');
+            headerDropdown.setAttribute('aria-expanded', 'false');
+        });
+    }
 });
+
