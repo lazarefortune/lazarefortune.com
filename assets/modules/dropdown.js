79286c88e0464892d0f3451d74d02b70251be930
Lazare Fortune <lazarefortune@gmail.com>
2025-01-29 15:17:48 +0100

    chore: Refactor dropdown functionality and enhance UI with improved alignment and icons.

--- assets/modules/dropdown.js
+++ assets/modules/dropdown.js
@@ -69,3 +69,23 @@ function closeAllDropdowns() {
 }
 
 document.addEventListener('DOMContentLoaded', initDropdowns);
+
+
+const headerDropdown = document.querySelector('#header-dropdown-button');
+const headerDropdownMenu = document.querySelector('#header-dropdown-menu');
+
+headerDropdown.addEventListener('click', (event) => {
+    event.stopPropagation(); // Empêche la propagation du clic au document
+
+    // Bascule la visibilité du menu déroulant associé
+    headerDropdownMenu.classList.toggle('is-visible');
+
+    // Met à jour l'attribut aria-expanded pour l'accessibilité
+    const isExpanded = headerDropdown.getAttribute('aria-expanded') === 'true';
+    headerDropdown.setAttribute('aria-expanded', !isExpanded);
+});
+
+document.addEventListener('click', () => {
+    headerDropdownMenu.classList.remove('is-visible');
+    headerDropdown.setAttribute('aria-expanded', 'false');
+});
