1651612e413dcfe48183c4c184661d971f2d9d15
Lazare Fortune <lazarefortune@gmail.com>
2025-03-04 01:23:41 +0100

    feat: Optimize newsletter subscription flow and enhance account management UI

--- src/Http/Controller/Newsletter/NewsletterSubscriberController.php
+++ src/Http/Controller/Newsletter/NewsletterSubscriberController.php
@@ -2,55 +2,34 @@
 
 namespace App\Http\Controller\Newsletter;
 
+use App\Domain\Auth\Core\Entity\User;
 use App\Domain\Auth\Core\Repository\UserRepository;
-use App\Domain\Newsletter\Entity\NewsletterSubscriber;
-use App\Domain\Newsletter\Form\NewsletterSubscriberType;
 use App\Domain\Newsletter\Repository\NewsletterSubscriberRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 class NewsletterSubscriberController extends AbstractController
 {
+
+    public function __construct(
+        private EntityManagerInterface $em,
+        private UserRepository $userRepository
+    ) {
+    }
+
     #[Route('/newsletter/inscription', name: 'newsletter_subscribe')]
-    public function subscribe(
-        Request $request,
-        EntityManagerInterface $em,
-        NewsletterSubscriberRepository $subscriberRepository
-    ): Response
+    #[IsGranted('ROLE_USER')]
+    public function subscribeUser(): Response
     {
-        $subscriber = new NewsletterSubscriber();
-
-        $form = $this->createForm(NewsletterSubscriberType::class, $subscriber);
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $existing = $subscriberRepository->findOneBy(['email' => $subscriber->getEmail()]);
-
-            if ($existing) {
-                if ($existing->isSubscribed()) {
-                    $this->addFlash('info', 'Vous êtes déjà abonné à la newsletter.');
-                } else {
-                    $existing->setSubscribed(true);
-                    $em->flush();
-                    $this->addFlash('success', 'Votre abonnement a été réactivé.');
-                }
-            } else {
-                $em->persist($subscriber);
-                $em->flush();
-                $this->addFlash('success', 'Vous êtes abonné à la newsletter!');
-            }
-
-            return $this->redirectToRoute('app_home');
-        }
-
-        // 5) Rendre la vue pour afficher le formulaire
-        return $this->render('newsletter/subscribe.html.twig', [
-            'form' => $form->createView(),
-        ]);
+        /** @var User $user */
+        $user = $this->getUser();
+        $user->setNewsletterSubscribed(true);
+        $this->em->flush();
+        return $this->redirectToRoute('app_account_profile');
     }
 
     #[Route('/newsletter/desinscription/subscriber/{token}', name: 'newsletter_unsubscribe_subscriber')]
