b5990a5233c1cb59e46e65087ec2776fe98accd0
Lazare Fortune <lazarefortune@gmail.com>
2025-03-27 14:00:47 +0100

    chore: Add FormationEditForm and improve formation management
    Introduced FormationEditForm for better modularity and flexibility in handling formation updates. Enhanced slug field handling, updated form layout in templates, and dispatched events for content updates. These changes improve maintainability and provide a better user experience when editing formations.

--- src/Http/Admin/Form/Formation/FormationEditForm.php
+++ src/Http/Admin/Form/Formation/FormationEditForm.php
@@ -1,26 +1,26 @@
 <?php
-namespace App\Http\Admin\Form\Course;
+
+namespace App\Http\Admin\Form\Formation;
 
 use App\Domain\Attachment\Type\AttachmentType;
 use App\Domain\Course\Entity\Course;
+use App\Domain\Course\Entity\Formation;
 use App\Domain\Course\Entity\Technology;
-use App\Http\Admin\Form\Field\TechnologiesType;
+use App\Http\Admin\Form\ChaptersForm;
 use App\Http\Admin\Form\Field\TechnologyChoiceType;
 use App\Http\Admin\Form\Field\UserChoiceType;
 use App\Http\Type\EditorType;
 use App\Http\Type\SwitchType;
-use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
 use Vich\UploaderBundle\Handler\UploadHandler;
 
-class CourseEditForm extends AbstractType
+class FormationEditForm extends AbstractType
 {
     public function __construct(
         private readonly EntityManagerInterface $entityManager,
@@ -29,8 +29,8 @@ class CourseEditForm extends AbstractType
 
     public function buildForm( FormBuilderInterface $builder, array $options ): void
     {
-        /** @var Course $course */
-        $course = $options['data'];
+        /** @var Formation $course */
+        $formation = $options['data'];
 
         $builder
             ->add('title', TextType::class, [
@@ -55,30 +55,6 @@ class CourseEditForm extends AbstractType
                     'class' => 'form-input'
                 ]
             ])
-            ->add('youtubeId', TextType::class, [
-                'required' => false,
-                'attr' => [
-                    'class' => 'form-input'
-                ]
-            ])
-            ->add('duration', TextType::class, [
-                'required' => false,
-                'attr' => [
-                    'readonly' => true,
-                    'class' => 'form-input'
-                ]
-            ])
-            ->add('enableSource', SwitchType::class, [
-                'label' => 'Activer / Désactiver le code source',
-                'mapped' => false,
-                'data' => !empty($options['data']->getSource()),
-            ])
-            ->add('sourceFile', FileType::class, [
-                'required' => false,
-                'attr' => [
-                    'class' => 'form-input'
-                ]
-            ])
             ->add('publishedAt', DateTimeType::class, [
                 'label' => 'Date de publication',
                 'widget' => 'single_text',
@@ -89,46 +65,52 @@ class CourseEditForm extends AbstractType
                 'attr' => [
                     'class' => 'flatpickr-datetime form-input',
                 ],
-                'help' => 'Si la date est future, la vidéo sera planifiée.',
+                'help' => 'Si la date est future, la playlist sera planifiée.',
             ])
-            ->add('online', SwitchType::class)
-            ->add('premium', SwitchType::class)
-            ->add('deprecatedBy', TextType::class, [
+            ->add('youtubePlaylist', TextType::class, [
                 'required' => false,
                 'attr' => [
                     'class' => 'form-input'
                 ]
             ])
-            ->add('content', EditorType::class)
+            ->add('online', SwitchType::class)
             ->add('image', AttachmentType::class)
-            ->add('youtubeThumbnail', AttachmentType::class)
             ->add('mainTechnologies', TechnologyChoiceType::class, [
                 'required' => false,
                 'mapped'   => false,
                 'data' => $this->entityManager->getRepository(Technology::class)->findBy([
-                    'id' => array_map(fn($t) => $t->getId(), $course->getMainTechnologies())
+                    'id' => array_map(fn($t) => $t->getId(), $formation->getMainTechnologies())
                 ]),
             ])
             ->add('secondaryTechnologies', TechnologyChoiceType::class, [
                 'required' => false,
                 'mapped'   => false,
                 'data' => $this->entityManager->getRepository(Technology::class)->findBy([
-                    'id' => array_map(fn($t) => $t->getId(), $course->getSecondaryTechnologies())
+                    'id' => array_map(fn($t) => $t->getId(), $formation->getSecondaryTechnologies())
                 ]),
+            ])
+            ->add('short', EditorType::class)
+            ->add('content', EditorType::class)
+            ->add('chapters', ChaptersForm::class)
+            ->add('deprecatedBy', TextType::class, [
+                'required' => false,
+                'attr' => [
+                    'class' => 'form-input'
+                ]
             ])
         ;
 
         $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
-            /** @var Course $course */
-            $course = $event->getData();
-            if (!$course) {
+            /** @var Formation $formation */
+            $formation = $event->getData();
+            if (!$formation) {
                 return;
             }
 
             $form = $event->getForm();
 
             // Si la date est déjà passée, on désactive le champ
-            if ($course->getPublishedAt() < new \DateTimeImmutable()) {
+            if ($formation->getPublishedAt() < new \DateTimeImmutable()) {
                 $form->add('publishedAt', DateTimeType::class, [
                     'label' => 'Date de publication',
                     'widget' => 'single_text',
@@ -142,31 +124,15 @@ class CourseEditForm extends AbstractType
                     'help' => 'Non modifiable car la date est passée.',
                 ]);
             }
-
-            // Si pas de YoutubeID => on supprime le champ duration
-            if (!$course->getYoutubeId()) {
-                $form->remove('duration');
-            }
         });
 
 
         $builder->addEventListener(FormEvents::POST_SUBMIT, function (FormEvent $event) {
-            /** @var Course $course */
-            $course = $event->getData();
+            /** @var Formation $formation */
+            $formation = $event->getData();
             $form = $event->getForm();
 
-            $course->setUpdatedAt(new \DateTimeImmutable());
-
-            $enableSource = $form->get('enableSource')->getData(); // bool
-
-            // Si false => on supprime le fichier
-            if (!$enableSource && $course->getSource()) {
-                $this->uploaderHandler->remove($course, 'sourceFile');
-            }
-
-            if ($course->isPremium() &&  ($course->getPublishedAt() > new \DateTimeImmutable())) {
-                $course->setPublishedAt( new \DateTimeImmutable() );
-            }
+            $formation->setUpdatedAt(new \DateTimeImmutable());
 
             // traitement techno
             $mainTechnologies = $form->get('mainTechnologies')->getData(); // depuis le formulaire
@@ -182,7 +148,7 @@ class CourseEditForm extends AbstractType
             }
 
             // Sync des technologies
-            $removedUsages = $course->syncTechnologies(
+            $removedUsages = $formation->syncTechnologies(
                 array_merge($mainTechnologies, $secondaryTechnologies)
             );
 
@@ -192,5 +158,4 @@ class CourseEditForm extends AbstractType
             }
         });
     }
-
 }
--- src/Http/Studio/Controller/FormationController.php
+++ src/Http/Studio/Controller/FormationController.php
@@ -9,7 +9,10 @@ use App\Domain\Application\Event\ContentUpdatedEvent;
 use App\Domain\Course\Entity\Formation;
 use App\Http\Admin\Controller\CrudController;
 use App\Http\Admin\Data\Crud\FormationCrudData;
+use App\Http\Admin\Form\Formation\FormationEditForm;
 use App\Http\Security\ContentVoter;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -60,12 +63,30 @@ final class FormationController extends CrudController
 
     #[Route(path: '/{id<\d+>}', name: 'edit', methods: ['POST', 'GET'])]
     #[IsGranted('ROLE_AUTHOR')]
-    public function edit(Formation $formation): Response
+    public function edit(Formation $formation, Request $request, EventDispatcherInterface $dispatcher): Response
     {
         $this->denyAccessUnlessGranted(ContentVoter::EDIT , $formation );
-        $data = (new FormationCrudData($formation))->setEntityManager($this->em);
 
-        return $this->crudEdit($data);
+        $oldFormation = clone $formation;
+        $form = $this->createForm( FormationEditForm::class, $formation );
+        $form->handleRequest( $request );
+
+        if ( $form->isSubmitted() && $form->isValid() ) {
+            $newFormation = $form->getData();
+            $this->em->flush();
+            $this->addFlash('success', 'La playlist a bien été modifié');
+
+            $dispatcher->dispatch(new ContentUpdatedEvent($oldFormation, $newFormation));
+        }
+
+        # $data = (new FormationCrudData($formation))->setEntityManager($this->em);
+
+        #return $this->crudEdit($data);
+
+        return $this->render('pages/studio/formation/edit.html.twig', [
+            'form' => $form->createView(),
+            'entity' => $formation,
+        ]);
     }
 
     #[Route(path: '/{id<\d+>}', name: 'delete', methods: ['DELETE'])]
