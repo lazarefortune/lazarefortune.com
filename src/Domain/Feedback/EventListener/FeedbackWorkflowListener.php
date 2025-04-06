7f3c99e51cd629a7689e9535bf8509c8d6fccc0d
Lazare Fortune <lazarefortune@gmail.com>
2025-04-06 17:32:58 +0200

    chore: Add FeedbackWorkflowListener to handle resolved feedback emails
    This commit introduces a new event listener that triggers an email notification when feedback is marked as resolved. It generates a personalized email using a Twig template and sends it to the user via the MailService. The template includes a summary of the feedback and a link to the user's account.

---
+++ src/Domain/Feedback/EventListener/FeedbackWorkflowListener.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Domain\Feedback\EventListener;
+
+use App\Domain\Feedback\Entity\Feedback;
+use App\Infrastructure\Mailing\MailService;
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Workflow\Event\Event;
+
+class FeedbackWorkflowListener
+{
+    public function __construct(
+        private readonly MailService $mailService,
+        private readonly UrlGeneratorInterface $urlGenerator,
+    ) {}
+
+    #[AsEventListener(event: 'workflow.feedback_status.completed.mark_resolved')]
+    public function onFeedbackResolved(Event $event): void
+    {
+        /** @var Feedback $feedback */
+        $feedback = $event->getSubject();
+
+        $accountUrl = $this->urlGenerator->generate('app_account_index', [], UrlGeneratorInterface::ABSOLUTE_URL);
+
+        $email = $this->mailService->createEmail('mails/feedback/resolved.twig', [
+            'feedback' => $feedback,
+            'account' => $accountUrl,
+        ])
+            ->to($feedback->getEmail())
+            ->subject('Merci pour votre retour, il est maintenant résolu')
+            ->priority(Email::PRIORITY_HIGH);
+
+        // Envoie l’email
+        $this->mailService->send($email);
+    }
+}
